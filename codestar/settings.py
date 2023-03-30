a5335ae29cbab62a2fc0b8b510434e72aa0df9f9
neerojstha <nirojstha@hotmail.com>
2023-03-30 09:55:19 +0000

    code error corrected

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nero-blog.herokuaoo,com', 'localhost']
+ALLOWED_HOSTS = ['nero-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
